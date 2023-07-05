8c73a261153e5b1aebbba384b20b8b188c43a737
M. Imam Renaldy Gumay <imam.renaldy26@gmail.com>
2023-07-05 21:41:02 +0700

    menambahkan soal 3 dan 4

---
+++ index.ts
@@ -0,0 +1,3 @@
+console.log("Hello World")
+// let nama = "M. Imam Renaldy Gumay"
+console.log(nama)
---
+++ problem_3.ts
@@ -0,0 +1,3 @@
+// input nama
+let nama : string = "M. Imam Renaldy Gumay";
+console.log("Hello "+nama+"! Saya Javascript bahasa yang sangat menyenangkan")
