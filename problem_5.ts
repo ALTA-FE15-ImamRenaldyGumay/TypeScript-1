d9d93957b5d580096a92d4a1e992bd4803e5b8df
M. Imam Renaldy Gumay <imam.renaldy26@gmail.com>
2023-07-05 21:57:24 +0700

    menambahkan problem 5

---
+++ problem_5.ts
@@ -0,0 +1,6 @@
+console.log("Program Menghitung Luas Permukaan Tabung")
+let T : number = 20
+let r : number = 4
+let pi : number = 3.14
+let luasPermukaanTabung : number = 2 * pi * r * (r + T)
+console.log("Hasil Perhitungan Luas Permukaan Tabung = "+luasPermukaanTabung)
