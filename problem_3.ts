1e5ac395e65b502e07af148ff70fc3d614a1bfc1
M. Imam Renaldy Gumay <imam.renaldy26@gmail.com>
2023-07-05 21:50:05 +0700

    mengubah soal 3 dan 4

--- problem_3.ts
+++ problem_3.ts
@@ -1,3 +1,4 @@
 // input nama
-let nama : string = "M. Imam Renaldy Gumay";
+console.log("Program Hello + Nama")
+let nama : string = "M. Imam Renaldy Gumay"
 console.log("Hello "+nama+"! Saya Javascript bahasa yang sangat menyenangkan")
---
+++ problem_4.ts
@@ -0,0 +1,5 @@
+console.log("Program Menghitung Luas Segitiga")
+let alas : number = 20
+let tinggi : number = 25
+let luasSegitiga : number = (alas * tinggi) / 2
+console.log("Luas Segitiga adalah = "+luasSegitiga+" cm")
