0da121a16c865bdb87404757e14777b10cde6c74
M. Imam Renaldy Gumay <imam.renaldy26@gmail.com>
2023-07-18 22:08:24 +0700

    membuat perubahan

--- index.ts
+++ index.ts
@@ -1,3 +1,21 @@
 console.log("Hello World")
 // let nama = "M. Imam Renaldy Gumay"
-console.log(nama)
+// console.log(nama)
+
+let members: Array<Object> = [
+    {
+        id: 1,
+        names: "upin",
+        age:3
+    },
+    {
+        id: 1,
+        names: "upin",
+        age:3
+    },
+    {
+        id: 1,
+        names: "upin",
+        age:3
+    },
+]
